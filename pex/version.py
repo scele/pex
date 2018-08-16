e97e780154698b3dc4d0a2baf82e141be5530a07
Lauri Peltonen <lpeltonen@nvidia.com>
2018-08-16 13:13:07 +0300

    HACK: Remove setuptools version capping
    We need to bundle a newer version of setuptools, which conflicts
    with the one that pex bundles into the .bootstrap directory.
    Hence removing the version capping so that we can build pex with
    the latest setuptools.

--- pex/version.py
+++ pex/version.py
@@ -6,6 +6,6 @@ __version__ = '1.4.5'
 # NB: If we upgrade to setuptools>=34 pex's bootstrap code in `PEXBuilder` will need an update to
 # include the `packaging` package in the `.bootstrap/` code since we use
 # `packaging.specifiers.SpecifierSet` - indirectly - through `pkg_resources.Requirement.specifier`.
-SETUPTOOLS_REQUIREMENT = 'setuptools>=20.3,<34.0'
+SETUPTOOLS_REQUIREMENT = 'setuptools>=20.3' # HACK(lpeltonen): Removed ',<34.0'
 
 WHEEL_REQUIREMENT = 'wheel>=0.26.0,<0.32.0'
